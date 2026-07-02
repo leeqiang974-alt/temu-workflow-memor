228b6a0cf4a9a02214269768e18b515498f616d0
小图 <xiaotu@ozon.seller>
2026-07-02 17:41:43 +0800

    Allow Temu plugin private network requests

--- tools/temu_control_panel.py
+++ tools/temu_control_panel.py
@@ -405,6 +405,7 @@ def _write_bytes(handler: BaseHTTPRequestHandler, status: int, body: bytes, cont
     handler.send_header("Content-Type", content_type)
     handler.send_header("Content-Length", str(len(body)))
     handler.send_header("Access-Control-Allow-Origin", "*")
+    handler.send_header("Access-Control-Allow-Private-Network", "true")
     handler.end_headers()
     handler.wfile.write(body)
 
@@ -508,8 +509,9 @@ class ControlPanelHandler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(204)
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Private-Network", "true")
         self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Request-Private-Network")
         self.end_headers()
 
     def do_GET(self):
